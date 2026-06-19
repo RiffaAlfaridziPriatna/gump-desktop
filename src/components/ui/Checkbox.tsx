e221081aab1f9dfba972e53fe20f7342867c9ee2
riffaap <riffaspenda@gmail.com>
2026-06-19 21:37:34 +0700

    feat: implement dynamic style, color, and children for Checkbox component

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,23 +1,47 @@
-import {TouchableOpacity} from 'react-native';
+import {StyleSheet, TouchableOpacity, ViewStyle} from 'react-native';
 import CheckboxChecked from '../../assets/images/checkbox_checked.svg';
 import CheckboxUnchecked from '../../assets/images/checkbox_unchecked.svg';
+import { colors } from '@lib/colors';
+import { ReactNode } from 'react';
 
 type CheckboxProps = {
   checked: boolean;
   onToggle: () => void;
   size?: number;
+  color?: string;
+  style?: ViewStyle;
+  children?: ReactNode;
 };
 
-export function Checkbox({checked, onToggle, size = 20}: CheckboxProps) {
+export function Checkbox({
+  checked,
+  onToggle,
+  size = 20,
+  color = colors.text,
+  style,
+  children,
+}: CheckboxProps) {
   return (
     <TouchableOpacity
       onPress={onToggle}
-      activeOpacity={0.7}>
+      activeOpacity={0.7}
+      style={[styles.container, style]}>
       {checked ? (
-        <CheckboxChecked width={size} height={size} />
+        <CheckboxChecked width={size} height={size} color={color} />
       ) : (
-        <CheckboxUnchecked width={size} height={size} />
+        <CheckboxUnchecked width={size} height={size} color={color} />
       )}
+      {children}
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    cursor: 'pointer',
+    userSelect: 'none',
+  },
+});
