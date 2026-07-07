732d3bd099cb4c651cfadb8df11bb25b1864a34b
riffaap <riffaspenda@gmail.com>
2026-07-07 09:45:47 +0700

    fix: restore Windows autolink discovery for svg and async-storage.
    Manual react-native.config project overrides bypass RNW dependency enrichment, leaving cppHeaders undefined and crashing autolink.

--- react-native.config.js
+++ react-native.config.js
@@ -9,35 +9,6 @@ module.exports = {
   },
   assets: ['./src/assets/fonts/'],
   dependencies: {
-    '@react-native-async-storage/async-storage': {
-      platforms: {
-        windows: {
-          sourceDir: 'windows',
-          solutionFile: null,
-          projects: [
-            {
-              projectFile:
-                'ReactNativeAsyncStorage/ReactNativeAsyncStorage.vcxproj',
-              directDependency: true,
-            },
-          ],
-        },
-      },
-    },
-    'react-native-svg': {
-      platforms: {
-        windows: {
-          sourceDir: 'windows',
-          solutionFile: null,
-          projects: [
-            {
-              projectFile: 'RNSVG/RNSVG.vcxproj',
-              directDependency: true,
-            },
-          ],
-        },
-      },
-    },
     'react-native-screens': {
       platforms: {
         macos: null,
